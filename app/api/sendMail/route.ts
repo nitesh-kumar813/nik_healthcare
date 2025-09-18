1192ea08854ab6cd085b8da10be8e5504b8dba4c
Nitesh kumar <niteshkumaryadav813@gmail.com>
2025-09-18 09:10:42 +0530

    ESlint warning solve

--- app/api/sendMail/route.ts
+++ app/api/sendMail/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+
 import { transporter } from '@/lib/mailer';
 
 export async function POST(req: Request) {
--- app/api/sendSms/route.ts
+++ app/api/sendSms/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+
 import { sendSMS } from '@/lib/sms';
 
 export async function POST(req: Request) {
